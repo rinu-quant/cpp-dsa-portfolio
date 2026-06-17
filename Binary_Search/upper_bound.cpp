b38e16cbf5796902a4ac1c427ae0dc914635488e
RINU P R <ranmarinu666@gmail.com>
2026-06-17 21:17:57 +0530

    cpp dsa solutions updated

---
+++ Binary_Search/upper_bound.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    int n , target , tmp ;
+
+    std::cout << "Enter the number of elements : " ;
+    std::cin >> n ;
+
+    std::cout << "Enter the target element : " ;
+    std::cin >> target ;
+
+    int left = 0 , right = n-1 ;
+
+    std::vector<int> nums ;
+    nums.reserve(n) ;
+
+    std::cout << "Enter the elements : \n" ;
+    for ( int i{} ; i<n ; ++i )
+    {
+        std::cin >> tmp ;
+        nums.push_back(tmp) ;
+    }
+
+    while (left <= right )
+    {
+        int mid = left + ( right - left )/2 ;
+
+        if ( nums[mid] > target )
+        {
+            right = mid - 1 ;
+        }
+        else
+        {
+            left = mid + 1 ;
+        }
+    }
+
+    if ( nums[left] > target )
+    {
+       std::cout << std::endl << left << std::endl ;
+    }
+    else
+    {
+       std::cout << std::endl << n << std::endl ;
+    }
+
+    return 0 ;
+}
+
